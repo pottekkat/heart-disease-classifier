8cf2aeac6caebd68f3b8836f61147a42b69b1c30
Navendu Pottekkat <navendupottekkat@gmail.com>
2020-03-03 19:43:10 +0530

    initial commit

---
+++ env/lib/python3.7/site-packages/scipy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM SCIPY SETUP.PY
+short_version = '1.4.1'
+version = '1.4.1'
+full_version = '1.4.1'
+git_revision = 'Unknown'
+release = True
+
+if not release:
+    version = full_version
---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/navendu/ML_Projects/heart-disease-classifier/env/lib"
+XML2_LIBS="-lxml2 -L/home/navendu/ML_Projects/heart-disease-classifier/env/lib -lz -L/home/navendu/ML_Projects/heart-disease-classifier/env/lib -llzma -lpthread -L/home/navendu/ML_Projects/heart-disease-classifier/env/lib  -lm "
+XML2_INCLUDEDIR="-I/home/navendu/ML_Projects/heart-disease-classifier/env/include/libxml2 -I/home/navendu/ML_Projects/heart-disease-classifier/env/include"
+MODULE_VERSION="xml2-2.9.9"
+
